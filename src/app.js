d9b7f1c99eefc5b9d83cd182fd4a6d0d317537ff
Sumanth Abbu <sumanthabbu1398@gmil.com>
2026-02-09 23:30:07 -0600

    api-calls

--- src/app.js
+++ src/app.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const app = express();
-app.use((req, res) => res.send("Hello from server"));
-app.use("/test", (req, res) => res.send("hello from test request"));
-app.use("/hello", (req, res) => res.send("hello from HELLO request"));
+// app.use((req, res) => res.send("Hello from server"));
+app.get("/user/:id/:name/:pswrd", (req, res) => {
+  console.log(req.params);
+  res.send({ name: "sumanth", ID: "113" });
+});
+app.post("/user", (req, res) => {
+  res.send("saved successfully");
+});
+app.delete("/user", (req, res) => {
+  res.send("deleted successfully");
+});
+app.use("/test", (req, res) => res.send("from TEST request"));
+
+app.use("/test/2", (req, res) => res.send("from TEST2 request"));
+
 app.listen(3000, () => console.log("app is listening on port 3000"));
