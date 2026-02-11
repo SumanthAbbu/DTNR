128e3885414e9192b4f5db515ae70a69973b8308
Sumanth Abbu <sumanthabbu1398@gmil.com>
2026-02-10 22:32:35 -0600

    developed CRUD APIs

--- src/app.js
+++ src/app.js
@@ -3,19 +3,75 @@ const { connectDB } = require("./config/dataBase.js");
 const User = require("./Models/user.js");
 const app = express();
 
+//middleware for parsing jsonvdata from req -  js object
+app.use(express.json());
+//-- POST -SIGNUP API --
 app.post("/signUp", async (req, res) => {
-  const user = new User({
-    firstName: "Ian",
-    lastName: "Bell",
-    userID: "Lbell@hotmail",
-  });
+  const user = new User(req.body);
   try {
     await user.save();
-    res.send("data posted successfully!");
+    res.send("data saved !");
   } catch (err) {
-    res.status(400).send("error saving the user");
+    res.status(400).send("user not saved");
   }
 });
+//-- GET - ONE USER AND ALL USERS(FEED) --
+app.get("/user", async (req, res) => {
+  // try {
+  //   const user = await User.findOne({ userID: req.body.userID });
+  //   if (!user) res.status(404).send("user not found");
+  //   else res.send(user);
+  // } catch (err) {
+  //   res.status(500).send("soemthing went wrong : internal server error");
+  // }
+  try {
+    const users = await User.find({ userID: req.body.userID });
+    if (users.length === 0) res.status(404).send("user not found");
+    else res.send(users);
+  } catch (err) {
+    res.status(500).send("soemthing went wrong : internal server error");
+  }
+});
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find();
+    if (users.length === 0) return res.status(404).send("users not found");
+    res.send(users);
+  } catch (err) {
+    res.status(500).send("soemthing went wrong : internal server error");
+  }
+});
+//-- DELETE API --
+app.delete("/user", async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.body.id);
+    res.send("deleted successfully!");
+  } catch (err) {
+    res.status(500).send("something went wrong : internal server error");
+  }
+});
+//--- UPDATE API ---
+app.patch("/user", async (req, res) => {
+  try {
+    await User.findByIdAndUpdate(req.body.id, req.body);
+    res.send("updated successfuly!");
+  } catch (err) {
+    res.status(500).send("something went wrong : internal server error");
+  }
+});
+// app.post("/signUp", async (req, res) => {
+//   const user = new User({
+//     firstName: "Ian",
+//     lastName: "Bell",
+//     userID: "Lbell@hotmail",
+//   });
+//   try {
+//     await user.save();
+//     res.send("data posted successfully!");
+//   } catch (err) {
+//     res.status(400).send("error saving the user");
+//   }
+// });
 connectDB()
   .then(() => {
     console.log("connection succesful to dataBase");
